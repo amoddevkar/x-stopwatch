ab961ac63b340031c9a6a20bedea16747d3c51a4
amoddevkar <87513080+amoddevkar@users.noreply.github.com>
2024-09-21 12:55:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  useEffect(() => {
+    let intervalId;
+    if (isRunning) {
+
+      intervalId = setInterval(() => setTime(time + 1), 10);
+    }
+    return () => clearInterval(intervalId);
+  }, [isRunning, time]);
+
+
+  const startAndStop = () => {
+    setIsRunning(!isRunning);
+  };
+
+
+  const reset = () => {
+    setTime(0);
+  };
+
+
+  const min = Math.floor((time % 360000) / 6000);
+
+
+  const sec = Math.floor((time % 6000) / 100);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Stopwatch</h1>
+      <span>Time : </span>
+      <span>{min.toString().padStart(2, "0")}:
+        {sec.toString().padStart(2, "0")}</span><br />
+      <button onClick={startAndStop}>
+        {isRunning ? "Stop" : "Start"}
+      </button>
+      <button onClick={reset}>
+        Reset
+      </button>
     </div>
   );
 }
